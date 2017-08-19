1bbf177e692952e9711233d3622c20bce969d90a
ngo.trung.thang <ngo.trung.thang@framgia.com>
2017-08-19 10:40:12 +0700

    dfdf

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
+var port1 = Number(process.env.PORT || 5000);
+var server = app.listen(port1, function () {
    var host = server.address().address
    var port = server.address().port
    
