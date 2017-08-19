1d08247da7ac26717b38157b30f06e0049ea08c3
ngo.trung.thang <ngo.trung.thang@framgia.com>
2017-08-19 12:22:29 +0700

    dfgd

--- index.js
+++ index.js
@@ -14,22 +14,24 @@ app.get('/webhook', function(req, res) {
 
 // Xử lý khi có người nhắn tin cho bot
 app.post('/webhook', function(req, res) {
-  // var entries = req.body.entry;
-  // for (var entry of entries) {
-  //   var messaging = entry.messaging;
-  //   for (var message of messaging) {
-  //     var senderId = message.sender.id;
-  //     if (message.message) {
-  //       // If user send text
-  //       if (message.message.text) {
-  //         var text = message.message.text;
-  //         console.log(text); // In tin nhắn người dùng
-  //         sendMessage(senderId, "Tui là bot đây: " + text);
-  //       }
-  //     }
-  //   }
-  // }
- console.log(req)
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); // In tin nhắn người dùng
+          sendMessage(senderId, "Tui là bot đây: " + text);
+        }
+      }
+    }
+  }
+ // console.log(req)
+ // sendMessage(senderId, "Tui là bot đây: " + text);
+ //  //       }
   res.status(200).send("OK");
 });
 
