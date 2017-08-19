ef06efce9f6ef1f8dd23c1bf53ccb0a4b0896db8
ngo.trung.thang <ngo.trung.thang@framgia.com>
2017-08-19 10:44:53 +0700

    dfdf

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 443);
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
 res.end('Hello Node.js\n');
