63e6fdc07ad346454264981c5614d10a52232688
ngo.trung.thang <ngo.trung.thang@framgia.com>
2017-08-19 10:55:36 +0700

    dfdfd

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+var logger = require('morgan');
 var http = require('http');
-var port = Number(process.env.PORT || 443);
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello Node.js\n');
-}).listen(port);
-console.log('Server running');
+var bodyParser = require('body-parser');
+var express = require('express');
+var request = require('request');
+var router = express();
+
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
+
+
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
+});
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
