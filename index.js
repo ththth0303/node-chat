99ffefb53eb69ade3c535de37f8c0341d9f8a1e0
ngo.trung.thang <ngo.trung.thang@framgia.com>
2017-08-19 11:30:30 +0700

    dfgd

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 })
 
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
+  if (req.query['hub.verify_token'] === 'anhthang') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
