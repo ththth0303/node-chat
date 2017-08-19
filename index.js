d6dc8bd2ea08ee883d4f80cc32beeeb17ef965a9
ngo.trung.thang <ngo.trung.thang@framgia.com>
2017-08-19 12:41:27 +0700

    dfgd

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+var http = require('http');
+var bodyParser = require('body-parser');
 var express = require('express');
+var router = express();
+
 var app = express();
 
-app.get('/', function (req, res) {
-   console.log('grgrgrg')
-   res.send('Hello World');
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
+var request = require("request");
 
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
+});
+
+// Đây là đoạn code để tạo Webhook
 app.get('/webhook', function(req, res) {
   if (req.query['hub.verify_token'] === 'anhthang') {
     res.send(req.query['hub.challenge']);
@@ -15,24 +26,22 @@ app.get('/webhook', function(req, res) {
 
 // Xử lý khi có người nhắn tin cho bot
 app.post('/webhook', function(req, res) {
-  // var entries = req.body.entry;
-  // for (var entry of entries) {
-  //   var messaging = entry.messaging;
-  //   for (var message of messaging) {
-  //     var senderId = message.sender.id;
-  //     if (message.message) {
-  //       // If user send text
-  //       if (message.message.text) {
-  //         var text = message.message.text;
-  //         console.log(text); // In tin nhắn người dùng
-  //         sendMessage(senderId, "Tui là bot đây: " + text);
-  //       }
-  //     }
-  //   }
-  // }
- console.log(req)
- // sendMessage(senderId, "Tui là bot đây: " + text);
- //  //       }
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); // In tin nhắn người dùng
+          sendMessage(senderId, "Tui là bot đây: " + text);
+        }
+      }
+    }
+  }
+
   res.status(200).send("OK");
 });
 
@@ -56,11 +65,9 @@ function sendMessage(senderId, message) {
   });
 }
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
-var port1 = Number(process.env.PORT || 5000);
-var server = app.listen(port1, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-   })
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
