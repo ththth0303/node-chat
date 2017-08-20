25279e7b892f25a870524fdc755e2b1717331185
ththth0303 <trungthangngo@gmail.com>
2017-08-20 15:19:25 +0700

    eefef

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ var dung = [
   'Dung xinh đẹp',
   '...',
 ]
+
+function convertTo(str) {
+        str = str.toLowerCase();
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+        str = str.replace(/đ/g, "d");
+        return str;
+}
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -43,7 +55,7 @@ app.post('/webhook', function(req, res) {
       if (message.message) {
         // If user send text
         if (message.message.text) {
-            switch(message.message.text.toLowerCase()) {
+            switch(convertTo(message.message.text)) {
                 case 'tuấn':
                     var text = 'Tuấn củ lìn'
                     break;
